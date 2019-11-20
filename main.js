bafb5ed2346e490cc2425a513edfcecdfd3c5f6f
jignesh-parekh-roche <jigneshkumar.parekh@roche.com>
2019-11-19 18:05:27 -0800

    Added search box

--- main.js
+++ main.js
@@ -7,18 +7,28 @@ const roomsMap = {
     x: 682,
     y: 70
   }
-}
-
+};
 
 function locateRoom() {
-  let urlParams = new URLSearchParams(window.location.search);
-  const room = urlParams.get('room');
+  const room = document.getElementById('search').value;
   console.log(`--> Locating the room ${room}`);
-  const roomKey = roomsMap[Object.keys(roomsMap).find(key => key.includes(room))];
-  console.log(`--> Room config: `, roomKey);
-  const roomEl = document.getElementsByClassName('room').item(0);
-  console.log(`--> roomEl: `, roomEl);
-  roomEl.classList.remove('hide');
-  roomEl.style.left = roomKey.x + 'px';
-  roomEl.style.top = roomKey.y + 'px';
+  const roomObj = roomsMap[Object.keys(roomsMap).find(key => key.includes(room))];
+  if (roomObj) {
+    const roomEl = document.getElementsByClassName('room').item(0);
+    roomEl.classList.remove('hide');
+    roomEl.style.left = roomObj.x + 'px';
+    roomEl.style.top = roomObj.y + 'px';
+  }
+}
+
+function locateYourself() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const x = urlParams.get('x'), y = urlParams.get('y');
+  console.log(`--> You're here: [${x}, ${y}]`);
+  if (x && y) {
+    const youAreHereEl = document.getElementsByClassName('you-are-here').item(0);
+    youAreHereEl.classList.remove('hide');
+    youAreHereEl.style.left = x + 'px';
+    youAreHereEl.style.top = y + 'px';
+  }
 }
